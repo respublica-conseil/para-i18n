1bb7f0eb78544113d8cb51e4dca7702d50438a15
xana68 <alex@glyph.fr>
2017-02-23 17:56:53 +0100

    add hint on btn

--- lib/para/i18n/resources_table.rb
+++ lib/para/i18n/resources_table.rb
@@ -13,7 +13,7 @@ module Para
         return unless resource.class.translates?
 
         path = component.relation_path(resource, :translation, action: :edit)
-        options = { class: 'btn btn-sm btn-icon-info btn-shadow' }
+        options = { class: 'btn btn-sm btn-icon-info btn-shadow hint--left', :'aria-label' => ::I18n.t('para.i18n.translate') }
 
         view.link_to(path, options) do
           content_tag(:i, '', class: 'fa fa-globe')
