dc2b175a0a9f3e753ce5479064bb79217d285455
vala <gonjo@free.fr>
2019-04-16 14:40:02 +0200

    allow basic para component to use translation module

--- lib/para/i18n/engine.rb
+++ lib/para/i18n/engine.rb
@@ -8,6 +8,10 @@ module Para
       end
 
       initializer 'para.i18n.extend_para_routes' do
+        ::Para.config.routes.extend_routes_for(:component) do
+          resource :translation, only: [:edit, :update], controller: '/para/admin/translations'
+        end
+        
         ::Para.config.routes.extend_routes_for(:crud_component) do
           resource :translation, only: [:edit, :update], controller: '/para/admin/translations'
         end
