a713277df2490d8730e7796a896fe810e85b2679
vala <gonjo@free.fr>
2019-06-18 16:49:14 +0200

    allow disabling i18n fallbacks

--- lib/para/i18n/fallbacks.rb
+++ lib/para/i18n/fallbacks.rb
@@ -1,8 +1,17 @@
 module Para
   module I18n
     module Fallbacks
+      mattr_accessor :_disable_fallbacks
+
+      def self.without_i18n_fallbacks(&block)
+        self._disable_fallbacks = true
+        block.call
+      ensure
+        self._disable_fallbacks = false
+      end
+
       def self.i18n_fallback_for(locale)
-        return unless ::I18n.respond_to?(:fallbacks)
+        return if _disable_fallbacks || !::I18n.respond_to?(:fallbacks)
 
         if (fallbacks = ::I18n.fallbacks[locale]) && fallbacks.length > 1
           fallbacks[1]
