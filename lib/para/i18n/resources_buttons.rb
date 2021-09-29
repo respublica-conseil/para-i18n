41c29d6dae43198136bbb17d9690db1c2b7a31b2
vala <gonjo@free.fr>
2021-09-29 20:52:58 +0200

    fix(table-action-button): add aria label, tooltip and use same markup as para resource action buttons

--- lib/para/i18n/resources_buttons.rb
+++ lib/para/i18n/resources_buttons.rb
@@ -5,7 +5,10 @@ module Para
         return unless resource.class.translates? && view.can?(:translate, resource)
 
         path = component.relation_path(resource, :translation, action: :edit)
-        options = { class: 'btn btn-default' }
+        options = {
+          class: 'btn btn-sm btn-icon-info btn-shadow hint--left',
+          aria: { label: I18n.t('para.i18n.translate') }
+        }
 
         view.link_to(path, options) do
           content_tag(:i, '', class: 'fa fa-globe')
