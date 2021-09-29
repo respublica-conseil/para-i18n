83d91569c19f68115b11d7de69a9df7066db93d1
vala <gonjo@free.fr>
2021-09-29 20:52:58 +0200

    fix(table-action-button): add aria label, tooltip and use same markup as para resource action buttons

---
+++ app/javascripts/index.js
@@ -0,0 +1,1 @@
+import "./para/i18n";
---
+++ dist/index.js
@@ -0,0 +1,19 @@
+(() => {
+  // app/javascripts/para/admin/translation.js
+  var TranslationLocaleSelect;
+  TranslationLocaleSelect = class TranslationLocaleSelect2 {
+    static onChange($field) {
+      var targetLocale, targetURL;
+      targetURL = $field.data("locale-select-target-url");
+      targetLocale = $field.val();
+      targetURL = targetURL.replace("target_locale=_", ["target_locale", targetLocale].join("="));
+      return Turbolinks.visit(targetURL.replace(":locale", targetLocale));
+    }
+  };
+  $(document).on("turbo:load turbo:frame-load", function() {
+    return $("body").on("change", "[data-locale-select]", function(e) {
+      return TranslationLocaleSelect.onChange($(e.currentTarget));
+    });
+  });
+})();
+//# sourceMappingURL=index.js.map
